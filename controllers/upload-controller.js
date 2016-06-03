c77e54ab82d2c64d625eaa67e1e3a04c0ded5ce7
sadibou22 <sadiboutra@gmail.com>
2016-06-03 14:38:31 -0400

    Update upload-controller.js

--- controllers/upload-controller.js
+++ controllers/upload-controller.js
@@ -3,13 +3,14 @@ var http = require('http');
 var express = require('express'),
 fs = require('fs');
 var csv=require('csv2json-convertor');//importing csv2json-
-var serviceConsultant = require('../Models/Consultant-Model.js');
+var serviceModel= require('../Models/Consultant-Model.js');
+var serviceConsultant = require('../services/Services-Consultant.js');
 var mongoose = require('mongoose');
 
 
 //get un consultant by id
 exports.getConsultant = function(id, callback) {
-    serviceConsultant.ConsultantModel.findById(id, function(err, consultant){
+    serviceModel.ConsultantModel.findById(id, function(err, consultant){
 		if(err) {
 			console.log(err);
 		} else if(consultant == null) {
@@ -17,12 +18,12 @@ exports.getConsultant = function(id, callback) {
 			callback.json(consultant);		
 		} else{callback.json(consultant); }
 	});
-    console.log('test jai trouv� un consultant avec son id');
+    //console.log('test jai trouv� un consultant avec son id');
 };
 
 //get tous les consultants
 exports.getAllConsultants = function(req, res) {
-    serviceConsultant.ConsultantModel.find(null)
+    serviceModel.ConsultantModel.find(null)
 	.exec(function(error, consultants){
 		if(error) {
 			res.send('error');
@@ -31,15 +32,23 @@ exports.getAllConsultants = function(req, res) {
 			res.json(consultants);
 		}
 	});
-    console.log('test Tous les consultants');
+    //console.log('test Tous les consultants');
 };
 
-//test upload
+//upload
 exports.upload = function(FileName){
-	var data1=csv.csvtojson(FileName); //csvtojson is function that accepts csv filenames and returns JSON object
-	console.log(data1);
-	//save data in mongo
-   	saveInMongo(data1);
+	//validation fichier avec l'extension
+	var valideExt = serviceConsultant.verifFileExtension(serviceConsultant.extensionsValides,FileName);
+	//console.log(valideExt);
+	if(valideExt){
+		console.log('Bon fichier '+valideExt);
+		var data1=csv.csvtojson(FileName); //csvtojson is function that accepts csv filenames and returns JSON object
+		console.log(data1);
+		//save data in mongo
+		saveInMongo(data1);
+	}else{
+		console.log('Sorry fichier invalide '+ valideExt);
+	}
 	//ensuite delete le file pour ne pas encombré a activer apres
 	fs.unlink(FileName, function(err) {
 		if (err) {
@@ -59,7 +68,7 @@ var saveInMongo = function (data){
 //fonction qui teste si consultant existe le save ou le update
 var findConsultant = function(data,j){
 	
-	var c = new serviceConsultant.ConsultantModel({
+	var c = new serviceModel.ConsultantModel({
 		
 		_id :  data[j].EmpId,
 		Prenom : data[j].Prenom,
@@ -70,17 +79,17 @@ var findConsultant = function(data,j){
 	//c.Competences.push(data[j].Competences);
 	//c.Projets.push(data[j].Projet);
 	
-	serviceConsultant.ConsultantModel.findById(data[j].EmpId, function(err, consultant){
+	serviceModel.ConsultantModel.findById(data[j].EmpId, function(err, consultant){
 		if(err) {
 			console.log(err);
 		} else if(consultant == null) {
-			console.log('Désolé, consultant inexistant donc save like insert');
+			//console.log('Désolé, consultant inexistant donc save like insert');
 			c.save(function (err) {
 			if(err) {console.log(err);throw err;}
-			console.log('saved!!!! yeah');
+			//console.log('saved!!!! yeah');
 			});
 		} else{
-			console.log('yes, consultant existe donc save like update');
+			//console.log('yes, consultant existe donc save like update');
 		 	c.update({
 		
 				//_id :  data[j].EmpId,
@@ -90,7 +99,7 @@ var findConsultant = function(data,j){
 				Projets : [data[j].Projet]
 				},function (err) {
 					if(err) {console.log(err);throw err;}
-					console.log('saved!!!! yeah');
+					//console.log('saved!!!! yeah');
 			}); 
 		}
 	});
